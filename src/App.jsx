c8b3d06382f7b67fae6f9bbe38f7a81cc21c7b1a
Najmul shaon <najmul.nh.shaon@gmail.com>
2023-10-30 22:32:12 +0600

    onClick event handler added

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,21 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const hadndleEventClick = () => {
+  //   alert("Btn Clicked");
+  // };
+  // function hadndleEventClick() {
+  //   alert("clicked");
+  // }
+
+  const hadndleEventClick = (num) => {
+    alert(num + 5);
+  };
 
   return (
     <>
       <h3>react-core-concept-part2</h3>
+      <button onClick={() => hadndleEventClick(5)}>Click Here</button>
     </>
   );
 }
